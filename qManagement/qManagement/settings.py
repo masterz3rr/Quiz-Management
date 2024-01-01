f30fab93baf97edf0a6bb99c84ef84ab547cfb2f
Masterzerr <alejerry24@gmail.com>
2024-01-01 23:32:06 +0800

    Update settings.py

--- qManagement/qManagement/settings.py
+++ qManagement/qManagement/settings.py
@@ -148,5 +148,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'jerry.ale@bisu.edu.ph'
-EMAIL_HOST_PASSWORD = 'jerryale24'
+EMAIL_HOST_USER = '#'
+EMAIL_HOST_PASSWORD = '#'
